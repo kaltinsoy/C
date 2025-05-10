191705c1dcb239f80b78e0afcfad0f41633924f7
k0rrluna <k0rr.luna1@gmail.com>
2025-05-10 03:04:01 +0300

    chapter5

--- DeitelC/Chapter5/guessTheNum.c
+++ DeitelC/Chapter5/guessTheNum.c
@@ -2,18 +2,41 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+void guessTheNum();
+
 int main(void) {
     srand(time(NULL));
-    int num = 1 + rand() % 999;
-    printf("%s\n%s\n%s\n", "I have a number between 1 and 1000.", "Can you guess my number?", "Please type your first guess.");
-    int scanNum = 0;
-    scanf("%d", &scanNum);
 
-    if((scanNum > 1001) || (scanNum < 0)) {
-        puts("Invalid integer!");
-    }
+    guessTheNum();
+    return 0;
 
-    while(scanNum != num) {
-        if("")
-    }
+}
+
+
+void guessTheNum()
+{
+    int num = 1 + rand() % 1000;
+        printf("%s\n%s\n%s\n", "I have a number between 1 and 1000.", "Can you guess my number?", "Please type your first guess.");
+        int scanNum = 0;
+        scanf("%d", &scanNum);
+        char con;
+
+        if((scanNum > 1001) || (scanNum < 0)) {
+            puts("Invalid integer!");
+        }
+
+        do {
+        while(scanNum != num) {
+            if(scanNum < num) {
+                puts("Too low!");
+                scanf("%d", &scanNum);
+            } else {
+                puts("Too high");
+                scanf("%d", &scanNum);
+            }
+        }
+        }
+        while(scanf(" %c", &con));
+        puts("FOUND!");
 }
